17f5be5ce53f5f3cde4042b86bc1465cdeab6f56
sanychiquetti <sanymara77@hotmail.com>
2023-05-14 08:44:59 -0300

    refatorando o código

--- cypress/e2e/barriga/functional.cy.js
+++ cypress/e2e/barriga/functional.cy.js
@@ -16,14 +16,20 @@ describe('Should test at a funcional level', () =>{
     it('Criando uma conta', () => {
 
         //inserir uma conta
-        cy.get('[data-test="menu-settings"]').click()
-        cy.get('[href="/contas"]').click()
-        cy.get('[data-test="nome"]').type('sany')
-        cy.get('.btn').click()
-        cy.get('.toast-success').should('contain', 'Conta inserida com sucesso')
+        cy.get('[data-test="menu-settings"]')
+            .click()
+        cy.get('[href="/contas"]')
+            .click()
+        cy.get('[data-test="nome"]')
+            .type('sany')
+        cy.get('.btn')
+            .click()
+        cy.get('.toast-success')
+            .should('contain', 'Conta inserida com sucesso')
 
         //alterar uma conta
-        cy.get('tr > :nth-child(2) > :nth-child(1) > .far').click()
+        cy.get('tr > :nth-child(2) > :nth-child(1) > .far')
+            .click()
         cy.get('.form-control')
             .clear()
             .click()
@@ -31,7 +37,8 @@ describe('Should test at a funcional level', () =>{
         cy.get('.btn').click()
 
         //criar conta repetida
-        cy.get('.form-control').type('conta alterada')
+        cy.get('.form-control')
+            .type('conta alterada')
         cy.get('.btn').click()
 
     })
