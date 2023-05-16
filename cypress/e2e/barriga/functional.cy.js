3341a0ff081fd72e420aac14aed49dc4e1539eef
sanychiquetti <sanymara77@hotmail.com>
2023-05-15 21:46:05 -0300

    criando locators

--- cypress/e2e/barriga/functional.cy.js
+++ cypress/e2e/barriga/functional.cy.js
@@ -1,51 +1,47 @@
 /// <reference types="cypress"/>
 
+import loc from '../../support/locators'
+import '../../support/commandsConta'
+
 describe('Should test at a funcional level', () => {
     before(() => {
-        cy.visit('https://barrigareact.wcaquino.me/')
-        cy.get('.input-group > .form-control').type('sanymara77@hotmail.com')
-        cy.get(':nth-child(2) > .form-control').type('Santope77')
-        cy.get('.btn').click()
-        cy.get('.toast-message').should('contain', 'Bem vindo')
+        cy.login('sanymara77@hotmail.com', 'Santope77')
+        cy.resetApp()
     })
 
     it('Create an account', () => {
-        cy.get('[data-test="menu-settings"]')
-            .click()
-        cy.get('[href="/contas"]')
-            .click()
-        cy.get('[data-test="nome"]')
-            .type('sany')
-        cy.get('.btn')
-            .click()
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta de teste')
         cy.get('.toast-success')
             .should('contain', 'Conta inserida com sucesso')
     })
 
-    it.only('Change an account', ()=>{
-        cy.get('[data-test="menu-settings"]')
+    it('Change an account', ()=>{
+        cy.acessarMenuConta()
+        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR)
             .click()
-        cy.get('[href="/contas"]')
-            .click()
-        cy.xpath("//table//td[contains(.,'sany')]/..//i[@class='far fa-edit']")
-            .click()
-        cy.get('[data-test="nome"]')
+        cy.get(loc.CONTAS.NOME)
             .clear()
-            .click()
             .type('conta alterada')
-        cy.get('.btn')
+        cy.get(loc.CONTAS.BTN_SALVAR)
             .click()
+        cy.get(loc.MESSAGE)
+            .should('contain', 'Conta atualizada com sucesso')
+    })
+    it('Cant Create repeated account', () => {
+        cy.acessarMenuConta()
+        cy.inserirConta()
+            .type('conta alterada')
+        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE)
+            .should('contain', 'status code 400')
     })
 
-    // it('Create repeated account', () => {
-    //     cy.get('[data-test="menu-settings"]')
-    //         .click()
-    //     cy.get('[href="/contas"]')
-    //         .click()
-    //     cy.get('.form-control')
-    //         .type('conta alterada')
-    //     cy.get('.btn').click()
-    //     cy.get('.toast-error > .toast-message')
-    //         .should('contain', 'status code 400')
-    // })
+    it.only('Should create a transaction', () => {
+        cy.get(loc.MENU.MOVIMENTACAO).click()
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
+        cy.get(loc.MOVIMENTACAO.VALOR).type('123')
+        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR_MOV).click()
+    })
 })
---
+++ cypress/support/locators.js
@@ -0,0 +1,30 @@
+const locators = {
+    LOGIN: {
+        USER: '[data-test="email"]',
+        PASSWORD: '[data-test=passwd]',
+        BTN_LOGIN: '.btn'
+    },
+    
+    MENU: {
+        SETTINGS: '[data-test=menu-settings]', 
+        CONTAS: '[href="/contas"]',
+        RESET: '[href="/reset"]',
+        MOVIMENTACAO: '[data-test=menu-movimentacao]'
+    },
+
+    CONTAS: {
+        NOME: '[data-test=nome]',
+        BTN_SALVAR: '.btn',
+        XP_BTN_ALTERAR: "//table//td[contains(.,'Conta de teste')]/..//i[@class='far fa-edit']"
+    },
+
+    MOVIMENTACAO: {
+        DESCRICAO: '[data-test=descricao]',
+        VALOR: '[data-test="valor"]',
+        INTERESSADO: '[data-test="envolvido"]',
+        BTN_SALVAR_MOV: '.btn-primary'
+    },
+    MESSAGE: '.toast-message'
+}
+
+export default locators
